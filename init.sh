ed5d9166f51c52bacc5b8cc81ae328c700d89bc7
reik2k <reik2k@hotmail.com>
2015-10-23 18:26:55 +0200

    subir script de inicializar GIT

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+git config --global user.name 'reik2k'
+git config --global user.email 'reik2k@hotmail.com'
+
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
