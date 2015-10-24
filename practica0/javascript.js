70d1f85060ee2f006a3af7c107fa1465f2405002
reik2k <reik2k@hotmail.com>
2015-10-24 13:51:07 +0200

    PRAC0: pruebas para cambiar las posiciones de la clasificación

--- practica0/javascript.js
+++ practica0/javascript.js
@@ -1,3 +1,13 @@
+//3.1
+
+//3.2)
 javascript:javascript:console.log(document.getElementById('calsificacion_completa').rows[2].style.background="green")
 
+//3.3)
 javascript:javascript:console.log(document.getElementsByClassName('pts seleccionado').item(1).textContent)
+
+var table=document.getElementById('calsificacion_completa');
+var rowFirst=table.rows[2];
+var rowLast=table.rows[21];
+
+table.insertRow(1).innerHTML='prueba';
