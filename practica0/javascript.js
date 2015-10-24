be6b71399d1a938dbba3f3e1b218fd7238a75594
reik2k <reik2k@hotmail.com>
2015-10-24 12:00:38 +0200

    practica0 Terminada

---
+++ practica0/javascript.js
@@ -0,0 +1,3 @@
+javascript:javascript:console.log(document.getElementById('calsificacion_completa').rows[2].style.background="green")
+
+javascript:javascript:console.log(document.getElementsByClassName('pts seleccionado').item(1).textContent)
