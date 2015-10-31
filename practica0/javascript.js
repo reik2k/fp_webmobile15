530690221551cff216afc19566dcd7ed6a7ef2b7
abel2k <reik2k@hotmail.com>
2015-10-31 20:23:00 +0100

    Terminar prac0

--- practica0/javascript.js
+++ practica0/javascript.js
@@ -1,13 +1,11 @@
 //3.1
+javascript:var rows=document.getElementsByTagName("tr");
+var table=document.getElementById("calsificacion_completa");
+rows[2].parentNode.insertBefore(rows[21], rows[2]);
 
 //3.2)
-javascript:javascript:console.log(document.getElementById('calsificacion_completa').rows[2].style.background="green")
+javascript:javascript:console.log(document.getElementById('calsificacion_completa').rows[2].style.background="green");
 
 //3.3)
-javascript:javascript:console.log(document.getElementsByClassName('pts seleccionado').item(1).textContent)
+javascript:javascript:console.log(document.getElementsByClassName('pts seleccionado').item(1).textContent);
 
-var table=document.getElementById('calsificacion_completa');
-var rowFirst=table.rows[2];
-var rowLast=table.rows[21];
-
-table.insertRow(1).innerHTML='prueba';
